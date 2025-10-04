bebc142858d457af5abdb8a5cbcd2353f47051b0
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-04 14:25:49 +0700

    node server

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node.js Server!' });
+  res.json({ message: 'Hello from Node.js Server!!!' });
 });
 
 app.listen(port, () => {
